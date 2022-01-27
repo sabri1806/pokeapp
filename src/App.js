627cf14627a408736e54e9e467ed8190aadb86af
Sabrina Lucero <sabrinita185@gmail.com>
2022-01-27 19:55:31 -0300

    Add language to context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import PokemonList from "./PokemonList";
+import LanguageContext from "./context/languageContext";
 
 function App() {
+  const [language, setLanguage] = useState("en");
+
   return (
-    <div className="App">
-      <PokemonList />
-    </div>
+    <LanguageContext.Provider value={language}>
+      <div className="App">
+        <header>
+          Frav-Pokedex
+          <div>
+            <select
+              onChange={(e) => setLanguage(e.target.value)}
+              value={language}
+            >
+              <option value="en">English</option>
+              <option value="es">Español</option>
+            </select>
+          </div>
+        </header>
+        <PokemonList />
+      </div>
+    </LanguageContext.Provider>
   );
 }
 
